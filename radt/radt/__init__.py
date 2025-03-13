34ece178e2610a8ab57660d98b2cf3f56162a166
sipondo <titr@itu.dk>
2025-03-13 13:15:04 +0000

    readme

--- radt/radt/__init__.py
+++ radt/radt/__init__.py
@@ -1,5 +1,5 @@
 """Resource-Aware Data systems Tracker (radT) for automatically tracking and training machine learning software."""
 
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 
 from .radt import cli
